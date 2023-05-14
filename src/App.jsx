d26a425d6b3cec688c25c49536c5017bfdb3ab65
nara04040 <nara04040@naver.com>
2023-05-14 20:35:41 +0900

    초기 세팅

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div id="app">
+      <h1>⚾ 숫자 야구 게임</h1>
+      <p>
+        <strong>1~9까지의 수</strong>를 중복없이
+        <strong>3개</strong> 입력해주세요. <br />
+        올바른 예) 139 <br />
+        틀린 예) 122
+      </p>
+      <form>
+        <input type="text" id="user-input" />
+        <button id="submit">확인</button>
+      </form>
+      <h3>📄 결과</h3>
+      <div id="result">1볼 1스트라이크</div>
+      <button id="game-restart-button">재시작</button>
+    </div>
+  );
+}
+
+export default App;
